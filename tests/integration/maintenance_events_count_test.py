99df16ca6048a34b4cb0d82d12f5786156ab1821
Taras Voronovich <taras.varanovich@gmail.com>
2026-01-14 19:49:20 +0100

    DEI-2-3 Maintenance events count

--- tests/integration/maintenance_events_count_test.py
+++ tests/integration/maintenance_events_count_test.py
@@ -12,7 +12,13 @@ def test():
     spark: SparkSession = session.make_csv_local()
     (read_file_from_resource(spark, MAINTENANCE_EVENTS)
      .createOrReplaceTempView(table_name))
-    df = spark.sql(f"SELECT COALESCE(SUM(downtime_min)) AS downtime_min FROM {table_name}")
-    result: int = df.collect()[0]["downtime_min"]
-    assert result == 6180
+    df = spark.sql(f"""
+        WITH maintenance_events AS (
+            SELECT DISTINCT event_id FROM {table_name}
+        )
+        SELECT COUNT(*) AS events_count
+        FROM maintenance_events
+    """)
+    result: int = df.collect()[0]["events_count"]
+    assert result == 94
     spark.stop()
