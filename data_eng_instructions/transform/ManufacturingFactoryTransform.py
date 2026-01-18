6fbc4b04f806072a1fb98e28a9ac246701b42253
Taras Voronovich <taras.varanovich@gmail.com>
2026-01-18 20:38:26 +0100

    DEI-3-2 Defect filter - test failed

--- data_eng_instructions/transform/ManufacturingFactoryTransform.py
+++ data_eng_instructions/transform/ManufacturingFactoryTransform.py
@@ -4,88 +4,94 @@ from pyspark.sql.types import *
 
 from data_eng_instructions.type.ManufacturingFactory import MANUFACTURING_FACTORY_CSV
 
+
 def csv_to_type(dataframe: DataFrame) -> DataFrame:
     assert dataframe.schema == MANUFACTURING_FACTORY_CSV
     return (dataframe
-            .withColumn("timestamp",
-                        when(col("timestamp").isNull() | (trim(col("timestamp")) == ""),
-                             raise_error("REQUIRED: timestamp cannot be null/empty"))
-                        .otherwise(to_timestamp(col("timestamp"), "yyyy-MM-dd HH:mm:ss")))
+    .withColumn("timestamp",
+                when(col("timestamp").isNull() | (trim(col("timestamp")) == ""),
+                     raise_error("REQUIRED: timestamp cannot be null/empty"))
+                .otherwise(to_timestamp(col("timestamp"), "yyyy-MM-dd HH:mm:ss")))
 
-            .withColumn("planned_qty",
-                        when(col("planned_qty").isNull() | (trim(col("planned_qty")) == ""),
-                             raise_error("REQUIRED: planned_qty must be valid integer"))
-                        .otherwise(col("planned_qty").cast(IntegerType())))
+    .withColumn("planned_qty",
+                when(col("planned_qty").isNull() | (trim(col("planned_qty")) == ""),
+                     raise_error("REQUIRED: planned_qty must be valid integer"))
+                .otherwise(col("planned_qty").cast(IntegerType())))
 
-            .withColumn("produced_qty",
-                        when(col("produced_qty").isNull() | (trim(col("produced_qty")) == ""),
-                             raise_error("REQUIRED: produced_qty must be valid integer"))
-                        .otherwise(col("produced_qty").cast(IntegerType())))
+    .withColumn("produced_qty",
+                when(col("produced_qty").isNull() | (trim(col("produced_qty")) == ""),
+                     raise_error("REQUIRED: produced_qty must be valid integer"))
+                .otherwise(col("produced_qty").cast(IntegerType())))
 
-            .withColumn("scrap_qty",
-                        when(col("scrap_qty").isNull() | (trim(col("scrap_qty")) == ""),
-                             raise_error("REQUIRED: scrap_qty must be valid integer"))
-                        .otherwise(col("scrap_qty").cast(IntegerType())))
+    .withColumn("scrap_qty",
+                when(col("scrap_qty").isNull() | (trim(col("scrap_qty")) == ""),
+                     raise_error("REQUIRED: scrap_qty must be valid integer"))
+                .otherwise(col("scrap_qty").cast(IntegerType())))
 
-            .withColumn("defects_count",
-                        when(col("defects_count").isNull() | (trim(col("defects_count")) == ""),
-                             raise_error("REQUIRED: defects_count must be valid integer"))
-                        .otherwise(col("defects_count").cast(IntegerType())))
+    .withColumn("defects_count",
+                when(col("defects_count").isNull() | (trim(col("defects_count")) == ""),
+                     raise_error("REQUIRED: defects_count must be valid integer"))
+                .otherwise(col("defects_count").cast(IntegerType())))
 
-            .withColumn("maintenance_due_date",
-                        when(col("maintenance_due_date").isNull() | (trim(col("maintenance_due_date")) == ""),
-                             raise_error("REQUIRED: maintenance_due_date must be valid YYYY-MM-DD"))
-                        .otherwise(to_date(col("maintenance_due_date"), "yyyy-MM-dd")))
+    .withColumn("maintenance_due_date",
+                when(col("maintenance_due_date").isNull() | (trim(col("maintenance_due_date")) == ""),
+                     raise_error("REQUIRED: maintenance_due_date must be valid YYYY-MM-DD"))
+                .otherwise(to_date(col("maintenance_due_date"), "yyyy-MM-dd")))
 
-            .withColumn("cycle_time_s", col("cycle_time_s").cast(FloatType()))
-            .withColumn("oee", col("oee").cast(FloatType()))
-            .withColumn("availability", col("availability").cast(FloatType()))
-            .withColumn("performance", col("performance").cast(FloatType()))
-            .withColumn("quality", col("quality").cast(FloatType()))
-            .withColumn("vibration_mm_s", col("vibration_mm_s").cast(FloatType()))
-            .withColumn("temperature_c", col("temperature_c").cast(FloatType()))
-            .withColumn("pressure_bar", col("pressure_bar").cast(FloatType()))
+    .withColumn("cycle_time_s", col("cycle_time_s").cast(FloatType()))
+    .withColumn("oee", col("oee").cast(FloatType()))
+    .withColumn("availability", col("availability").cast(FloatType()))
+    .withColumn("performance", col("performance").cast(FloatType()))
+    .withColumn("quality", col("quality").cast(FloatType()))
+    .withColumn("vibration_mm_s", col("vibration_mm_s").cast(FloatType()))
+    .withColumn("temperature_c", col("temperature_c").cast(FloatType()))
+    .withColumn("pressure_bar", col("pressure_bar").cast(FloatType()))
 
-            .withColumn("defect_type",
-                        coalesce(col("defect_type"), lit(None).cast(StringType())))
-            .withColumn("downtime_reason",
-                        coalesce(col("downtime_reason"), lit(None).cast(StringType())))
+    .withColumn("defect_type",
+                coalesce(col("defect_type"), lit(None).cast(StringType())))
+    .withColumn("downtime_reason",
+                coalesce(col("downtime_reason"), lit(None).cast(StringType())))
 
-            .withColumn("factory_id", trim(col("factory_id")))
-            .withColumn("line_id", trim(col("line_id")))
-            .withColumn("shift", trim(col("shift")))
-            .withColumn("product_id", trim(col("product_id")))
-            .withColumn("order_id", trim(col("order_id")))
-            .withColumn("machine_state", trim(col("machine_state")))
-            .withColumn("maintenance_type", trim(col("maintenance_type")))
-            .withColumn("operator_id", trim(col("operator_id")))
-            .withColumn("workorder_status", trim(col("workorder_status")))
+    .withColumn("factory_id", trim(col("factory_id")))
+    .withColumn("line_id", trim(col("line_id")))
+    .withColumn("shift", trim(col("shift")))
+    .withColumn("product_id", trim(col("product_id")))
+    .withColumn("order_id", trim(col("order_id")))
+    .withColumn("machine_state", trim(col("machine_state")))
+    .withColumn("maintenance_type", trim(col("maintenance_type")))
+    .withColumn("operator_id", trim(col("operator_id")))
+    .withColumn("workorder_status", trim(col("workorder_status")))
 
-            .select(
-                    "timestamp",
-                    "factory_id",
-                    "line_id",
-                    "shift",
-                    "product_id",
-                    "order_id",
-                    "planned_qty",
-                    "produced_qty",
-                    "scrap_qty",
-                    "defects_count",
-                    "defect_type",
-                    "cycle_time_s",
-                    "oee",
-                    "availability",
-                    "performance",
-                    "quality",
-                    "machine_state",
-                    "downtime_reason",
-                    "maintenance_type",
-                    "maintenance_due_date",
-                    "vibration_mm_s",
-                    "temperature_c",
-                    "pressure_bar",
-                    "operator_id",
-                    "workorder_status"
-            )
-        )
+    .select(
+        "timestamp",
+        "factory_id",
+        "line_id",
+        "shift",
+        "product_id",
+        "order_id",
+        "planned_qty",
+        "produced_qty",
+        "scrap_qty",
+        "defects_count",
+        "defect_type",
+        "cycle_time_s",
+        "oee",
+        "availability",
+        "performance",
+        "quality",
+        "machine_state",
+        "downtime_reason",
+        "maintenance_type",
+        "maintenance_due_date",
+        "vibration_mm_s",
+        "temperature_c",
+        "pressure_bar",
+        "operator_id",
+        "workorder_status"
+    )
+    )
+
+
+def filter_out_invalid_defects(dataframe: DataFrame) -> DataFrame:
+    assert dataframe.schema == MANUFACTURING_FACTORY_CSV
+    return dataframe.filter((col("defect_type").isNotNull() & (col("defect_count") > 0)))
--- tests/unit/transform/manufactoring_factory_transform_test.py
+++ tests/unit/transform/manufactoring_factory_transform_test.py
@@ -1,45 +1,89 @@
 from pyspark.sql import SparkSession
 
-from data_eng_instructions.transform.ManufacturingFactoryTransform import csv_to_type
-from data_eng_instructions.type.ManufacturingFactory import MANUFACTURING_FACTORY_CSV, MANUFACTURING_FACTORY
+from data_eng_instructions.transform.ManufacturingFactoryTransform import csv_to_type, filter_out_invalid_defects
+from data_eng_instructions.type.ManufacturingFactory import MANUFACTURING_FACTORY_CSV
 
+valid_row = [
+    {
+        "timestamp": "2025-11-03 06:00:00",
+        "factory_id": "FRA-PLANT-01",
+        "line_id": "Line-A",
+        "shift": "Shift-1",
+        "product_id": "P-Widget",
+        "order_id": "WO-20251103-1860",
+        "planned_qty": 47,
+        "produced_qty": 41,
+        "scrap_qty": 1,
+        "defects_count": 0,
+        "defect_type": None,
+        "cycle_time_s": 44.4,
+        "oee": 0.986,
+        "availability": 1.0,
+        "performance": 1.033,
+        "quality": 0.954,
+        "machine_state": "Running",
+        "downtime_reason": None,
+        "maintenance_type": "Corrective",
+        "maintenance_due_date": "2025-11-05",
+        "vibration_mm_s": 1.814,
+        "temperature_c": 40.02,
+        "pressure_bar": 6.36,
+        "energy_kwh": 11.31,
+        "operator_id": "OP-015",
+        "workorder_status": "In-Progress"
+    }
+]
+
+defect_example = [
+    {
+        "timestamp": "2025-11-03 07:15:00",
+        "factory_id": "FRA-PLANT-01",
+        "line_id": "Line-C",
+        "shift": "Shift-1",
+        "product_id": "P-Gadget",
+        "order_id": "WO-20251103-8509",
+        "planned_qty": 45,
+        "produced_qty": 43,
+        "scrap_qty": 2,
+        "defects_count": 2,
+        "defect_type": None,
+        "cycle_time_s": 34.15,
+        "oee": 0.975,
+        "availability": 1.0,
+        "performance": 1.037,
+        "quality": 0.941,
+        "machine_state": "Running",
+        "downtime_reason": None,
+        "maintenance_type": "Preventive",
+        "maintenance_due_date": "2025-11-04",
+        "vibration_mm_s": 1.704,
+        "temperature_c": 36.98,
+        "pressure_bar": 7.0,
+        "energy_kwh": 11.267,
+        "operator_id": "OP-017",
+        "workorder_status": "In-Progress"
+    }
+]
 
 def test_transform_csv_to_type():
     spark = SparkSession.builder.appName("Testing CSV to Type transform").getOrCreate()
-    sample_data = [
-        {
-            "timestamp": "2025-11-03 06:00:00",
-            "factory_id": "FRA-PLANT-01",
-            "line_id": "Line-A",
-            "shift": "Shift-1",
-            "product_id": "P-Widget",
-            "order_id": "WO-20251103-1860",
-            "planned_qty": 47,
-            "produced_qty": 41,
-            "scrap_qty": 1,
-            "defects_count": 0,
-            "defect_type": None,
-            "cycle_time_s": 44.4,
-            "oee": 0.986,
-            "availability": 1.0,
-            "performance": 1.033,
-            "quality": 0.954,
-            "machine_state": "Running",
-            "downtime_reason": None,
-            "maintenance_type": "Corrective",
-            "maintenance_due_date": "2025-11-05",
-            "vibration_mm_s": 1.814,
-            "temperature_c": 40.02,
-            "pressure_bar": 6.36,
-            "energy_kwh": 11.31,
-            "operator_id": "OP-015",
-            "workorder_status": "In-Progress"
-        }
-    ]
 
-    df = spark.createDataFrame(sample_data, MANUFACTURING_FACTORY_CSV)
+
+    df = spark.createDataFrame(valid_row, MANUFACTURING_FACTORY_CSV)
     result = df.transform(csv_to_type)
-    #assert result.schema == MANUFACTURING_FACTORY
+    # assert result.schema == MANUFACTURING_FACTORY
+    result.show()
+
+    assert result.count() == len(valid_row)
+
+
+def test_filter_out_invalid_defects():
+    spark = SparkSession.builder.appName("Testing CSV to Type transform").getOrCreate()
+
+    sample_data = valid_row + defect_example
+    df = spark.createDataFrame(sample_data, MANUFACTURING_FACTORY_CSV)
+    result = df.transform(filter_out_invalid_defects)
+    assert result.schema == MANUFACTURING_FACTORY_CSV
     result.show()
 
     assert result.count() == len(sample_data)
