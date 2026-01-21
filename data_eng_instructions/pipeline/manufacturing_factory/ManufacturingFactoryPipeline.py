b6f172a34f8f79dd6fefdb4ef2e81b2a9c573839
Taras Voronovich <taras.varanovich@gmail.com>
2026-01-21 15:52:56 +0100

    DEI-3-3 Added order id assignation

--- data_eng_instructions/pipeline/manufacturing_factory/ManufacturingFactoryPipeline.py
+++ data_eng_instructions/pipeline/manufacturing_factory/ManufacturingFactoryPipeline.py
@@ -1,12 +1,19 @@
 from typing import Any
 
+from pandas.core.interchange.dataframe_protocol import DataFrame
+from pyspark.sql import functions as F
 from pyspark.sql.connect.session import SparkSession
 
+from data_eng_instructions.constant.stubs import DEFAULT_ID
 from data_eng_instructions.filedefinition.FileType import FileType
+from data_eng_instructions.filedefinition.manufctoringfactory.source.ManufacturingFactoryDefinitionSource import \
+    ManufacturingFactoryDefinitionSource
 from data_eng_instructions.filedefinition.order.dwh.OrderDefinitionDWH import OrderDefinitionDWH
 from data_eng_instructions.pipeline.Pipeline import Pipeline
 from data_eng_instructions.pipeline.PipelineParam import PipelineParam
+from data_eng_instructions.reader.ManufacturingFactoryReader import ManufacturingFactoryReader
 from data_eng_instructions.reader.OrderReader import OrderReader
+from data_eng_instructions.transform.ManufacturingFactoryTransform import csv_to_type
 
 
 class ManufacturingFactoryPipeline(Pipeline):
@@ -17,9 +24,31 @@ class ManufacturingFactoryPipeline(Pipeline):
 
     def run(self) -> Any:
         print("Running Defect Pipeline - Manufacturing Factory")
-        print("Read orders:")
+
         file_type: FileType = self._param.get_result_type()
         spark: SparkSession = self._param.get_spark()
+
+        print("Read orders:")
         order_definition: OrderDefinitionDWH = OrderDefinitionDWH(file_type)
         order_reader: OrderReader = OrderReader(spark, order_definition)
-        order_reader.read_from_storage().show(10)
+        order_df: DataFrame = order_reader.read_from_storage()
+
+        print("Read manufacturing factories:")
+        mf_definition: ManufacturingFactoryDefinitionSource = ManufacturingFactoryDefinitionSource()
+        mf_df: DataFrame = (ManufacturingFactoryReader(spark, mf_definition)
+                            .read_batch()).transform(csv_to_type)
+
+        mf_df_fact = (
+            mf_df.alias("mf")
+            .join(
+                order_df.alias("od"),
+                F.col("mf.order_id") == F.col("od.order_natural_key"),
+                how="left"
+            )
+            .withColumn(
+                "order_id",
+                F.coalesce(F.col("od.order_id"), F.lit(DEFAULT_ID))
+            )
+            .drop("order_natural_key", "od.order_id")
+        )
+        mf_df_fact.show(10)
